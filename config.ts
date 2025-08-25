58c55703a24267902f1647b7d4e9dcec901413fb
jonatas <jonatas.mfreitas@detran.go.gov.br>
2025-08-24 22:50:37 -0300

    V 1.1.0

---
+++ config.ts
@@ -0,0 +1,61 @@
+export default {
+
+  LINKEDIN_EMAIL: "jonataskalebe900@gmail.com",
+  LINKEDIN_PASSWORD: "Machocado2#",
+
+
+  KEYWORDS: "Software Engineer Java Spring Boot Angular Flutter",
+  LOCATION: "Worldwide",
+  WORKPLACE: {
+    REMOTE: true,
+    ON_SITE: false,
+    HYBRID: false,
+  },
+  JOB_TITLE: "(Software Engineer|Software Developer|Full Stack|Full-Stack|Backend|Frontend|Java Developer|Angular Developer|Flutter Developer|Pleno Developer|Mid-Level)",
+  JOB_DESCRIPTION: "^(?=.*(java|spring|angular|flutter|aws|docker|microservices|rest|kafka|kubernetes))((?!(bachelor|degree|graduaç|formado|formação|bacharel|superior completo|6\\+ years|7\\+ years|8\\+ years|10\\+ years|principal|staff engineer|based in|located in|must be in|must reside in|eligible to work in|authorized to work in|work authorization in|U\\.S\\.|United States|Canada|U\\.K\\.|United Kingdom|European Union|EU))(.|\\n|\\r))*$",  JOB_DESCRIPTION_LANGUAGES: ["english", "french"],
+
+
+  PHONE: "+5562993442104",
+  CV_PATH: "C:\\Trabalho\\Resume\\CV_Jonatas-ENG.pdf",
+  COVER_LETTER_PATH: "C:\\Caminho\\Completo\\Para\\Sua\\CoverLetter.pdf",
+  HOME_CITY: "Goiânia, Brazil",
+
+
+  YEARS_OF_EXPERIENCE: {
+    "java": 4,
+    "spring": 4,
+    "angular": 4,
+    "flutter": 2,
+    "typescript": 4,
+    "aws": 3,
+    "docker": 3,
+    "kubernetes": 2,
+    "sql": 4,
+    "git": 4,
+    "rest api": 4
+  },
+
+
+  LANGUAGE_PROFICIENCY: {
+    "english": "professional",
+    "french": "basic"
+  },
+
+
+  REQUIRES_VISA_SPONSORSHIP: false,
+  TEXT_FIELDS: { "salary": "60k" },
+  BOOLEANS: {
+    "(bachelor|degree|bacharel|graduaç|formação|superior)": false,
+    "(authorized|eligible|work permit).*brazil": true,
+    "(authorized|eligible|work permit).*(USA|Canada|Europe)": false,
+    "sponsorship": false
+  },
+
+
+  MULTIPLE_CHOICE_FIELDS: {
+    "pronouns": "He/him"
+  },
+
+
+  SINGLE_PAGE: false,
+}
